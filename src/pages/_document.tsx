727ea34199c42d8f2cb38cbf3688312a58e32c4d
shelbey25 <sbyousey@crimson.ua.edu>
2026-02-04 15:37:35 -0600

    updated web icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ class MyDocument extends Document {
           {/* Ensure mobile devices use the device width for proper scaling */}
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           {/* Primary favicon (cache-busted) */}
-          <link rel="icon" href="/favicon.ico?v=1" />
+          <link rel="icon" href="/orb2.ico?v=1" />
           {/* Helpful additional links */}
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=1" />
